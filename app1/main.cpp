8d8d3baed866698670747a9681763b08e0491560
heartinice <ims1018@mail.ru>
2024-11-22 18:43:07 +0300

    Изменила программу основную

--- app1/main.cpp
+++ app1/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main() {
     std::cout << "Hello, Docker!" << std::endl;
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
     return 0;
 }
+
