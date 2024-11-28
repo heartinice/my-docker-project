5f32e423df38ea5117172fbc3711367b6122e838
heartinice <ims1018@mail.ru>
2024-11-28 08:01:46 +0300

    Полная замена проекта

--- database/base1.sql
+++ database/base1.sql
@@ -1,14 +1,18 @@
-CREATE DATABASE IF NOT EXISTS example_db;
+-- CREATE DATABASE IF NOT EXISTS example_db;
 
-USE example_db;
+-- USE example_db;
 
--- Добавьте ваши таблицы, данные или другие SQL-команды ниже.
+-- -- Добавьте ваши таблицы, данные или другие SQL-команды ниже.
 
-CREATE DATABASE example_db;
-USE example_db;
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
+-- CREATE DATABASE example_db;
+-- USE example_db;
+-- CREATE TABLE users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100),
+--     email VARCHAR(100)
+-- );
+-- INSERT INTO users (name, email) VALUES ('John Doe', 'john@example.com');
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT NOT NULL
 );
-INSERT INTO users (name, email) VALUES ('John Doe', 'john@example.com');
